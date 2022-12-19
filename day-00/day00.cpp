b2c656fe719aa60e696cceb17d4171f142f3f034
vishwaskaranth <109661229+vishwaskaranth@users.noreply.github.com>
2022-12-19 16:27:59 +0530

    day: 0 done

---
+++ day-00/day00.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{char name;
+int a;
+    cout<<"name";
+    cin>>name;
+    if (name=a)
+   { cout<<"hello sjec";
+   }
+   else
+   {
+    cout<<name;
+   }
+return 0 ;
+}
