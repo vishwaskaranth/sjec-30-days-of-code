8c07dcabd06ee3208aeec213821c5f6e4af1667e
vishwaskaranth <109661229+vishwaskaranth@users.noreply.github.com>
2022-12-23 19:40:57 +0530

    completed day -04

---
+++ day-04/day-04.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{int m,n,size;
+cout<<"enter the no of: ";
+cin>>m>>n;
+
+size=m*n;
+if(size%3==0)
+{cout<<"yes";
+}
+else{cout<<"no";
+}
+return 0;
+
+}
